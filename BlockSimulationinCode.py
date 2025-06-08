efe9e12c445e4eaf9aeec1f0c33ec70a5ce57d4a
Vaibhav777777 <86874858+Vaibhav777777@users.noreply.github.com>
2025-06-08 20:08:35 +0530

    Update BlockSimulationinCode.py

--- BlockSimulationinCode.py
+++ BlockSimulationinCode.py
@@ -69,3 +69,57 @@ display_chain(blockchain)
 
 print("Validity Check:\n")
 check_chain_validity(blockchain)
+
+# OUTPUT
+Original Blockchain: 
+
+
+        Block 0:
+        Timestamp     : 1749393346.947217
+        Data          : Genesis Block
+        Previous Hash : 0
+        Hash          : 7e0431b850efaeaf41a911c5f23d44682893b456f94fc26e5917dc2cbec203a5
+
+
+        Block 1:
+        Timestamp     : 1749393347.018393
+        Data          : Block 1 data
+        Previous Hash : 7e0431b850efaeaf41a911c5f23d44682893b456f94fc26e5917dc2cbec203a5
+        Hash          : df754b4f21f229d7c20f90522f68176daeedc4734f1a667fea3b216ed7b12379
+
+
+        Block 2:
+        Timestamp     : 1749393347.019385
+        Data          : Block 2 data
+        Previous Hash : df754b4f21f229d7c20f90522f68176daeedc4734f1a667fea3b216ed7b12379
+        Hash          : f83eed47beb8c72787aade78fb101277c2266c34792bb95d2d397cd2ead31c23
+
+Tampering Block 1...
+
+Blockchain After Tampering:
+
+
+        Block 0:
+        Timestamp     : 1749393346.947217
+        Data          : Genesis Block
+        Previous Hash : 0
+        Hash          : 7e0431b850efaeaf41a911c5f23d44682893b456f94fc26e5917dc2cbec203a5
+
+
+        Block 1:
+        Timestamp     : 1749393347.018393
+        Data          : Tampered data
+        Previous Hash : 7e0431b850efaeaf41a911c5f23d44682893b456f94fc26e5917dc2cbec203a5
+        Hash          : 1b3eb3a8ad1d988ea950c120dbf6beb6a7c912f72a79abb4c62952397858e2d0
+
+
+        Block 2:
+        Timestamp     : 1749393347.019385
+        Data          : Block 2 data
+        Previous Hash : df754b4f21f229d7c20f90522f68176daeedc4734f1a667fea3b216ed7b12379
+        Hash          : f83eed47beb8c72787aade78fb101277c2266c34792bb95d2d397cd2ead31c23
+
+Validity Check:
+
+Block 1 is valid.
+⚠️ Block 2 is INVALID due to broken hash link!
